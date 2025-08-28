624f85778b1f9dfb52981de4728ba5a8b28e823e
Ehtesham-19 <mehteshamdeveloper@gmail.com>
2025-08-28 14:40:48 +0500

    28/8

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,23 +1,13 @@
-import { useEffect } from "react";
 import "./Home.css";
 
 function Card({ img, heading, btnText }) {
-   
-   
-    useEffect(() => {
-         console.log("updated")
-    },[img])
-   
   return (
     <div className="card">
       <div className="img">
-              <img
-                  src={img}
-          alt="clothes"
-        />
+        <img src={img} alt="clothes" />
       </div>
-          <h2 className="title">{ heading}</h2>
-          <button>{ btnText}</button>
+      <h2 className="title">{heading}</h2>
+      <button>{btnText}</button>
     </div>
   );
 }
