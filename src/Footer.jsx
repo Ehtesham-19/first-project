3936ca0ee67b516c1f772c9ae17f6d3c96831047
Ehtesham-19 <mehteshamdeveloper@gmail.com>
2025-09-02 11:17:44 +0500

    1/9

---
+++ src/Footer.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <p
+      style={{
+        background: "black",
+        color: "white",
+        position: "fixed",
+        bottom: "0",
+        width: "100%",
+        margin: "0",
+        padding: "0",
+        textAlign: "center",
+        display: "inline-block",
+      }}
+    >
+      All Rights Are Reserved
+    </p>
+  );
+}
+export default Footer;
